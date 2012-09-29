9c4d80487d2659cd7eed57db63adf43feb40ab0f
Liam Gladdy <liam@gladdy.co.uk>
2012-09-29 20:22:11 +0100

    Adding core file

---
+++ twitter-access-control.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Twitter Access Control
+Plugin URI: https://github.com/lgladdy/wordpress-twitter-access-control
+Description: Limit access to a wordpress blog to only followers of a specific twitter account.
+Author: Liam Gladdy
+Version: 1.0
+Author URI: http://www.gladdy.co.uk
+*/
